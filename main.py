8aa674c148a6cf48171349a7011bd9de8df4f17d
andrew_marles <andrew@expatria.ca>
2023-03-09 17:33:31 -0800

    Messing around with stuff.

--- main.py
+++ main.py
@@ -7,10 +7,30 @@ import _thread
 
 from neopixel import Neopixel
 
+from umodbus.serial import ModbusRTU
+
 #while True:
 #    time.sleep(1)
 
 tick_timer_period = 20 # Hz
+systick = 0
+
+# the following definition is for a RP2
+rtu_pins = (Pin(12), Pin(13))     # (TX, RX)
+uart_id = 0
+
+slave_addr = 10             # address on bus as client
+
+client = ModbusRTU(
+    addr=slave_addr,        # address on bus
+    pins=rtu_pins,          # given as tuple (TX, RX)
+    baudrate=115200,        # optional, default 9600
+    data_bits=8,          # optional, default 8
+    stop_bits=1,          # optional, default 1
+    parity=None,          # optional, default None
+    ctrl_pin=26,          # optional, control DE/RE
+    uart_id=uart_id         # optional, default 1, see port specific documentation
+)
 
 # set up the I2C for screen
 sdaPIN=machine.Pin(14)
@@ -23,6 +43,7 @@ display.init_display()
 # Print "Hello world!" 1 pixel from top left, in colour 1 (on)
 # .text(text, x, y, c)
 display.contrast(255)  # bright
+emptyline =    "                 "
 displayline1 = "This is line 1"
 displayline2 = "This is line 2"
 displayline3 = "This is line 3"
@@ -71,6 +92,7 @@ led_update_counter = 0
 
 # Main Timer ISR
 def tick(timer):                # we will receive the timer object when being called
+    global systick
     global onboard_led_count
     global led
     global screen_update_counter, led_update_counter
@@ -88,6 +110,8 @@ def tick(timer):                # we will receive the timer object when being ca
     if led_update_counter < 0:
         led_update_counter = 0
         
+    systick = systick + 1
+        
 tim.init(freq=tick_timer_period, mode=Timer.PERIODIC, callback=tick)  # 50ms timer period
 
 # Main Loop - this will be used to poll jog buttons and joystick for continuous movement
@@ -98,15 +122,21 @@ while True:
         led_update_counter = led_update_period
         
     if screen_update_counter == 0:
-        if (bufferline1 != displayline1) | (bufferline2 != displayline2) | (bufferline2 != displayline2) :
+        if (bufferline1 != displayline1) | (bufferline2 != displayline2) | (bufferline3 != displayline3) :
             bufferline1 = displayline1
             bufferline2 = displayline2
             bufferline3 = displayline3
-            display.init_display()
+            display.fill(0)
+            #display.text(emptyline, 1, 1, 1)
+            #display.text(emptyline, 1, 10, 1)
+            #display.text(emptyline, 1, 20, 1)
+            
             display.text(bufferline1, 1, 1, 1)
             display.text(bufferline2, 1, 10, 1)
             display.text(bufferline3, 1, 20, 1)
             display.show()
         screen_update_counter = screen_update_period
+    
+    displayline3 = "tick " + str(systick)
         
         
